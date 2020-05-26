3cf36f9c94c1c7a2986c54500d0a23e9694a49c0
Paulo Sacilotti <paulo.sacilotti@gmail.com>
2020-05-26 15:29:11 -0300

    create new tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #CREATE
+  #get `/tasks/new`, to: `tasks#new`
+  #READ
+ # get `/tasks`, to: `tasks#index`
+  #UPDATE
+  #DELETE
 end
